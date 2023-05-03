ce0d8e6e633d053b1a00506f3d668d5b92649e1d
bloomboom <72567591+huseyin33@users.noreply.github.com>
2023-05-02 21:34:17 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,73 @@
+import {Component} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  ipAddress: string = '';
+  gasLimit: number = 80000;
+  fee: number = 2000;
+  containerClicked: boolean = false;
+  nameSpaceId: string = '';
+  data: string = '';
+  downloadLink: string | undefined;
+
+  constructor(private http: HttpClient) {
+  }
+
+  //creating name space id and data.
+  async generateRandomHex(len: number): Promise<string> {
+    const bytes = new Uint8Array(len);
+    window.crypto.getRandomValues(bytes);
+    return Array.from(bytes)
+      .map((byte) => byte.toString(16).padStart(2, '0'))
+      .join('');
+  }
+
+  async generateRandomMessage(): Promise<string> {
+    const len = Math.floor(Math.random() * 100);
+    return this.generateRandomHex(len);
+  }
+
+  async generateNameAndData(): Promise<void> {
+    let namespaceID = await this.generateRandomHex(8);
+    const data = await this.generateRandomMessage();
+    this.nameSpaceId = namespaceID;
+    this.data = data;
+
+
+  }
+
+  downloadResponseAsTextFile(content: string | undefined): void {
+    if (!content) {
+      console.error('Empty content, cannot download as a text file.');
+      return;
+    }
+    const blob = new Blob([content], {type: 'text/plain;charset=utf-8'});
+    const url = URL.createObjectURL(blob);
+    this.downloadLink = url;
+  }
+
+  async sendPFB(): Promise<void> {
+    const url = `http://${this.ipAddress}:26659/submit_pfb`;
+    const data = {
+      namespace_id: this.nameSpaceId,
+      data: this.data,
+      gas_limit: this.gasLimit,
+      fee: this.fee,
+    };
+    try {
+      const response = await this.http.post(url, JSON.stringify(data), { responseType: 'text'}).toPromise();
+      this.downloadResponseAsTextFile(response)
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Response:'+ error);
+
+    }
+  }
+
+
+}
